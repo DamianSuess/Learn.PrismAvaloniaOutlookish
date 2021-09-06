1f568e28234fe9dc5a2d5f3a88ae3e30094afc2b
Damian Suess <djsuess@gmail.com>
2021-09-06 08:30:47 -0400

    base items for notifications. Warning, needs Prism 8.1

--- source/SampleApp.Main/ViewModels/DashboardViewModel.cs
+++ source/SampleApp.Main/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Regions;
+﻿using Avalonia.Controls.Notifications;
+using Prism.Commands;
+using Prism.Regions;
 using SampleApp.Common;
 
 namespace SampleApp.ViewModels
@@ -6,12 +8,39 @@ namespace SampleApp.ViewModels
   public class DashboardViewModel : ViewModelBase
   {
     private IRegionManager _regionManager;
+    ////private IManagedNotificationManager _notifications;
 
-    public DashboardViewModel(IRegionManager regionManager)
+    public DashboardViewModel(IRegionManager regionManager) ////, IManagedNotificationManager notifyMngr)
     {
       _regionManager = regionManager;
+      ////_notifications = notifyMngr;
 
       Title = "Dashboard - No New Messages";
     }
+
+    ////public DelegateCommand CommandTestNotification => new DelegateCommand(OnNotification);
+    ////
+    ////private void OnNotification()
+    ////{
+    ////  _notifications.Show(
+    ////    new Avalonia.Controls.Notifications.Notification(
+    ////      "Welcome",
+    ////      "Sample Message",
+    ////      NotificationType.Information));
+    ////
+    ////  // Sample from Avalonia
+    ///   // NOTE
+    ///   //  - MainWindow.xaml.cs needs the WindowNotificationManager initialized first.
+    ////  ////ShowManagedNotificationCommand = MiniCommand.Create(() =>
+    ////  ////{
+    ////  ////  NotificationManager.Show(new Avalonia.Controls.Notifications.Notification("Welcome", "Avalonia now supports Notifications.", NotificationType.Information));
+    ////  ////});
+    ////
+    ////  ////ShowCustomManagedNotificationCommand = MiniCommand.Create(() =>
+    ////  ////{
+    ////  ////  NotificationManager.Show(new NotificationViewModel(NotificationManager) { Title = "Hey There!", Message = "Did you know that Avalonia now supports Custom In-Window Notifications?" });
+    ////  ////});
+    ////  
+    ////}
   }
 }
--- source/SampleApp.Main/Views/MainWindow.axaml.cs
+++ source/SampleApp.Main/Views/MainWindow.axaml.cs
@@ -1,17 +1,35 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
 using Avalonia.Markup.Xaml;
+using Prism.Ioc;
 
 namespace SampleApp.Views
 {
   public partial class MainWindow : Window
   {
+    private WindowNotificationManager _notificationArea;
+
     public MainWindow()
     {
       InitializeComponent();
 #if DEBUG
       this.AttachDevTools();
 #endif
+
+      // SAMPLE Avalonia Notifications
+      // To wire-up Notifications, we'll need Prism 8.1 to access ContainerLocator.
+
+      _notificationArea = new WindowNotificationManager(this) {
+        Position = NotificationPosition.TopRight,
+        MaxItems = 3
+      };
+
+      // Avalonia sample passes this along.
+      // However, we'll add it to our Prism Container ;)
+      //// DataContext = new MainWindowViewModel(_notificationArea);
+      //
+      // _regionNavigationService = ContainerLocator.Container.Resolve<IRegionNavigationService>();
     }
 
     private void InitializeComponent()
