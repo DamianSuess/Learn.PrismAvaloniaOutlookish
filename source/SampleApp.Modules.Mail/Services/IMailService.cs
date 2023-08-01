fdb2219ac20534b153130c302053c8571c72d575
Damian Suess <djsuess@gmail.com>
2023-08-01 14:44:24 -0400

    Added TabControl to Mail View. Code cleanup and XAML styler

--- source/SampleApp.Modules.Mail/Services/IMailService.cs
+++ source/SampleApp.Modules.Mail/Services/IMailService.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
 using SampleApp.Modules.Mail.Models;
 
-namespace SampleApp.Modules.Mail.Services
+namespace SampleApp.Modules.Mail.Services;
+
+public interface IMailService
 {
-  public interface IMailService
-  {
-    IEnumerable<MailMessage> Messages { get; set; }
+  IEnumerable<MailMessage> Messages { get; set; }
 
-    void GetMessages(int messageId);
+  void GetMessages(int messageId);
 
-    void Send(MailMessage message);
-  }
+  void Send(MailMessage message);
 }
--- source/SampleApp.Modules.Mail/Services/MailService.cs
+++ source/SampleApp.Modules.Mail/Services/MailService.cs
@@ -1,47 +1,47 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using SampleApp.Modules.Mail.Models;
 
-namespace SampleApp.Modules.Mail.Services
+namespace SampleApp.Modules.Mail.Services;
+
+public class MailService : IMailService
 {
-  public class MailService : IMailService
+  public IEnumerable<MailMessage> Messages { get; set; } = new ObservableCollection<MailMessage>()
   {
-    public IEnumerable<MailMessage> Messages { get; set; } = new ObservableCollection<MailMessage>()
-    {
-        new MailMessage()
-        {
-          From = "sample1.name@SuessLabs.com",
-          Subject = "Hello Prism.Avalonia",
-          Content = "Lorem ipsum does it thing",
-        },
-        new MailMessage()
-        {
-          From = "another2@xenoinc.com",
-          Subject = "RE: Hello Prism.Avalonia",
-          Content = "Lorem ipsum does it thing"
-        },
-        new MailMessage()
-        {
-          From = "sample3@SuessLabs.com",
-          Subject = "Hello Prism.Avalonia",
-          Content = "RE: Lorem ipsum does it thing",
-        },
-        new MailMessage()
-        {
-          From = "another4@xenoinc.com",
-          Subject = "RE: Hello Prism.Avalonia",
-          Content = "Lorem ipsum does it thing"
-        },
-      };
+      new MailMessage()
+      {
+        From = "sample1.name@SuessLabs.com",
+        Subject = "Hello Prism.Avalonia",
+        Content = "Lorem ipsum does it thing",
+      },
+      new MailMessage()
+      {
+        From = "another2@xenoinc.com",
+        Subject = "RE: Hello Prism.Avalonia",
+        Content = $"Lorem ipsum does it thing.{Environment.NewLine}Reply From: another2{Environment.NewLine}I'm replying back"
+      },
+      new MailMessage()
+      {
+        From = "sample3@SuessLabs.com",
+        Subject = "Hello Prism.Avalonia",
+        Content = "Lovely day using Prism.Avalonia isn't it?",
+      },
+      new MailMessage()
+      {
+        From = "another4@xenoinc.com",
+        Subject = "RE: Hello Prism.Avalonia",
+        Content = $"Lovely day using Prism.Avalonia isn't it?{Environment.NewLine}Reply From: another4{Environment.NewLine}I'm replying back"
+      },
+    };
 
-    public void GetMessages(int messageId)
-    {
-      throw new System.NotImplementedException();
-    }
+  public void GetMessages(int messageId)
+  {
+    throw new System.NotImplementedException();
+  }
 
-    public void Send(MailMessage message)
-    {
-      throw new System.NotImplementedException();
-    }
+  public void Send(MailMessage message)
+  {
+    throw new System.NotImplementedException();
   }
 }
